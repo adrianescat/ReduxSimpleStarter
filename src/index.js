aabb58b625e24fbcfca37eb33d26ed6a7ddc5e0b
adrianescat <adrian.escat@gmail.com>
2017-04-13 16:33:27 -0300

    Exporting and importing a new component.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from  'react'; //Core library. Para crear/manejar los componentes.
-import ReactDOM from  'react-dom'; //Para renderizar en el DOM.
+import React from  'react';
+import ReactDOM from  'react-dom';
+import SearchBar from './components/searchBar';
 
-//Creo un componente. Este debe producir HTML.
+const API_KEY = 'AIzaSyCAMpuGgKd1fHnymj4N27qRMFm-i0s64A4';
+
 const App = () => {
-    return <div>Hello World!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
-/* El HTML generado lo meto en la página (DOM)
- * params:
- * <App > - instancia de la clase App. Debemos pasar una instancia y no la clase.
- * DomElement - Pasamos el elemento donde debe renderizar el HTML generado.
- */
 ReactDOM.render(<App />, document.querySelector('.container'));
