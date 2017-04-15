aeef3bbcc0b9b1f0c7a08bf8b0281394c5bf730e
adrianescat <adrian.escat@gmail.com>
2017-04-15 11:00:55 -0300

    Using youtube api. Refactor index.js using react-class component. App state with ajax data

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from  'react';
+import React, { Component } from  'react';
 import ReactDOM from  'react-dom';
 import SearchBar from './components/searchBar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyCAMpuGgKd1fHnymj4N27qRMFm-i0s64A4';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'Nicky Jam'}, videos => {
+            this.setState({videos});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    } 
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
